c09c59d2f2747470ae048d461728e9f8a6af8a46
Elizabeth McDaniel <emcdaniel@wisc.edu>
2020-10-23 16:05:58 -0500

    fixing inStrain queue exec

--- executables/inStrain-profile-queue.sh
+++ executables/inStrain-profile-queue.sh
@@ -10,15 +10,18 @@ PYTHONPATH=''
 genome=$1
 code=$2
 genomePath=/home/GLBRCORG/emcdaniel/obscurePOS/metagenomes/finalBins/bins
-input=$genomePath/$genome.fa
-annotationPath=/home/GLBRCORG/emcdaniel/obscurePOS/metagenomes/finalBins/annotations
+
+strainsPath=/home/GLBRCORG/emcdaniel/obscurePOS/metagenomes/finalBins/strains
 
 # setup
 cd /home/GLBRCORG/emcdaniel/obscurePOS/metagenomes/finalBins/strains
 
 # profile each genome for 2015-07-16 sample
+# Mapped competitively to all genomes using bowtie2
+# Called genes from the combined genome file with Prodigal - this is the genes file
+# Individually queue by each genome to calculate individual microdiversity metrics by genome, provide scaffold to bin (STB) file for this 
 
-inStrain profile /home/GLBRCORG/emcdaniel/obscurePOS/metagenomes/finalBins/strains/POS-2015-07-16-mapping.sorted.bam $input -o results/$code.IS -p 8 -g $annotationPath/$genome/$genome.ffn 
+inStrain profile $strainsPath/POS-2015-07-16-mapping.sorted.bam $genomePath/$genome.fa -o results/$code.IS -p 8 -g $strains/all-all-POS-genomes-genes.fna -s POS-scaffolds-to-bins.stb
 
 
 
